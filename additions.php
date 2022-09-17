75dadeea58dba172ff22289470d68062d33110f5
Vladlen <vladli2000610@gmail.com>
2022-09-17 21:31:41 +0600

    Initial commit. Basic classes(AbstractAdvert, EstateAdvert), enums

---
+++ additions.php
@@ -0,0 +1,19 @@
+<?php
+    enum Category: string{
+        case Sale = "sale";
+        case Rent = "rent";
+    }
+
+
+    enum Type: string{
+        case House = "dom";
+        case Flat = "kvartira";
+    }
+
+
+    enum Period: string{
+        case Month = "month";
+        case Day = "day";
+
+    }
+?>

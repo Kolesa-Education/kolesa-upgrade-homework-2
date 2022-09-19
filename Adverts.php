88f05b8784053f5feeec91c1f397c86c48ad83ad
Dilmurat Zairakhunov <dlol96@mail.ru>
2022-09-19 23:03:15 +0600

    отредактировал

--- Adverts.php
+++ Adverts.php
@@ -1,47 +1,68 @@
 <?php
 require_once "AdvertsInterface.php";
+
 class Adverts
 {
-    private $arr;
-    public function __construct($arr)
+    private array $arr;
+
+    public function __construct(array $arr)
     {
         $this->arr = $arr;
     }
 
-    protected function checkPrice() :string
+    protected function checkPrice(): string
     {
-        if ($this->arr["price"] > 9999999)
-            return $this->arr["price"]/1000000 . " млн";
-        else return  $this->arr['price'] . " тг";
+        if ($this->arr["price"] > 9999999) {
+            return $this->arr["price"] / 1000000 . " млн";
+        } else {
+            return $this->arr['price'] . " тг";
+        }
     }
 
-    protected function checkCategory() : string
+    protected function checkCategory(): string
     {
-        if (array_search("rent", $this->arr)) return "Сдам";
-        else return "Продам";
+        if (array_search("rent", $this->arr)) {
+            return "Сдам";
+        } else {
+            return "Продам";
+        }
     }
 
     protected function checkPeriod(): string
     {
         if (array_key_exists("period", $this->arr)) {
-            if($this->arr['period']=="month") return " в месяц";
-            else return " в сутки";
+            if ($this->arr['period'] == "month") {
+                return " в месяц";
+            } else {
+                return " в сутки";
+            }
         }
         return "";
     }
-    protected  function checkType() : string {
-        if($this->arr["type"]=="kvartira") return "квартиру";
-        else return "дом";
-    }
-    protected function checkRooms(){
-        if($this->arr['type']=="kvartira") return "комнатную";
-        else return "комнатный";
+
+    protected
+    function checkType(): string
+    {
+        if ($this->arr["type"] == "kvartira") {
+            return "квартиру";
+        } else {
+            return "дом";
+        }
     }
 
-    public function getTitle() : void
+    protected function checkRooms()
     {
-        $result = $this->checkCategory() ." " .$this->arr['rooms']."-" .$this->checkRooms()." " . $this->checkType() ." за " .$this->checkPrice() .$this->checkPeriod();
-        echo $result .PHP_EOL;
+        if ($this->arr['type'] == "kvartira") {
+            return "комнатную";
+        } else {
+            return "комнатный";
+        }
+    }
+
+    public function getTitle(): void
+    {
+        $result = $this->checkCategory() . " " . $this->arr['rooms'] . "-" . $this->checkRooms() . " " . $this->checkType() . " за " . $this->checkPrice() . $this->checkPeriod();
+        echo $result . PHP_EOL;
     }
 
 

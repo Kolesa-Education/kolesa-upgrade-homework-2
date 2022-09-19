30d565018efa49bf496f4584c4bfe76e6f8e9f89
Andrey <Andreyshabalin34@gmail.com>
2022-09-19 22:47:25 +0600

    Правки согласно PR

--- index.php
+++ index.php
@@ -1,73 +1,92 @@
 <?php
-#Я думал сделать разбиение классов по типу жилья(дом/квартира) или по категории(аренда/полная продажа)
-#Решил остановиться на первом варианте, т.к. данный вариант показался мне более реалистичным и логичным
-#Если всё таки лучше будет выбрать вариант 2, то готов переделать)
+/*
+Я думал сделать разбиение классов по типу жилья(дом/квартира) или по категории(аренда/полная продажа)
+Решил остановиться на первом варианте, т.к. данный вариант показался мне более реалистичным и логичным
+Если всё таки лучше будет выбрать вариант 2, то готов переделать)
+*/
 
+/** 
+* Абстрактный класс, в котором объявил все поля; конструктор, который наследуют 2 дочерних класса; геттеры и сеттеры;
+* +метод по выбору дня через switch/case
+**/
 abstract class Advert
-#абстрактный класс, в котором объявил все поля; конструктор, который наследуют 2 дочерних класса; геттеры и сеттеры;
-# + метод по выбору дня через switch/case
-{
-    protected $rooms;        #кол-во комнат
-    protected $category;     #категория на продажу или аренду
-    protected $price;        #Цена
-    protected $type;         #Тип жилья (Квартира/Дом)
-    protected $period;       #Период в случае если это аренда
 
-    function __construct(...$args) {
-        #Конструктор, где помимо присвоения, я обрабатываю отсутствие периода, если происходит покупка а не аренда
-        $this->rooms = $args[0];
-        $this->category = $args[1];
-        $this->price = $args[2];
-        $this->type = $args[3];
-        if(isset($args[4])){
-        $this->period =  $args[4];
+{
+    protected int    $rooms;     //Кол-во комнат
+    protected string $category;  //Категория на продажу или аренду
+    protected string $price;     //Цена
+    protected string $type;      //Тип жилья (Квартира/Дом)
+    protected string $period;    //Период в случае если это аренда
+
+    public function __construct($args) 
+    {
+        //Конструктор, где помимо присвоения, я обрабатываю отсутствие периода, если происходит покупка а не аренда
+        $this->rooms = $args['rooms'];
+        $this->category = $args['category'];
+        $this->price = $args['price'];
+        $this->type = $args['type'];
+        if(isset($args['period']))
+        {
+        $this->period =  $args['period'];
         }
     }
     
-   #геттеры и сеттеры, лишним не будет
-   public function getRooms(){
+   //геттеры и сеттеры, лишним не будет
+   public function getRooms(): int
+   {
     return $this->rooms;
    }
 
-   public function setRooms($rooms){
+   public function setRooms($rooms): int
+   {
     return $this->rooms = $rooms;
    }
 
-   public function getCategory(){
+   public function getCategory(): string
+   {
     return $this->category;
    }
 
-   public function setCategory($category){
+   public function setCategory($category): string
+   {
     return $this->category = $category;
    }
-   public function getPrice(){
+   public function getPrice(): int
+   {
     return $this->price;
    }
 
-   public function setPrice($price){
+   public function setPrice($price): int
+   {
     return $this->price = $price;
    }
-   public function getType(){
+
+   public function getType(): string
+   {
     return $this->type;
    }
 
-   public function setType($type){
+   public function setType($type): string
+   {
     return $this->type = $type;
    }
-   public function getPeriod(){
+   public function getPeriod(): string
+   {
     return $this->period;
    }
 
-   public function setPeriod($period){
+   public function setPeriod($period): string
+   {
     return $this->period = $period;
    }
    
-   #абстрактный метод, который реализуем в дочерних классах
+   //Абстрактный метод, который реализуем в дочерних классах
     abstract function getTitle();
 
 
-    #Доп. функционал по подстановке слова, если геттеров и сеттеров не хватит))
-    function countPeriodWord(){
+    //Доп. функционал по подстановке слова, если геттеров и сеттеров не хватит))
+    function countPeriodWord(): string
+    {
         switch ($this->period){
             case "month":
                 $period_word = "месяц";
@@ -87,14 +106,12 @@ abstract class Advert
     }
 
 }
-class flatAdvert extends Advert{
- #Класс оъявлений для квартиры, включает в себя и покупку, и аренду   
-    function __construct(...$args) {
-        parent::__construct(...$args);   
-   }
-    
-    function getTitle(){
-        #Реализация вывода объявления, условие для полной покупки, или аренды
+class FlatAdvert extends Advert{
+ //Класс оъявлений для квартиры, включает в себя и покупку, и аренду   
+   
+    function getTitle()
+    {
+        //Реализация вывода объявления, условие для полной покупки, или аренды
         if ($this->category == 'sale') {
             echo "Продам $this->rooms-комнатную квартиру за $this->price тенге" . PHP_EOL;
         } elseif ($this->category == "rent"){
@@ -103,23 +120,21 @@ class flatAdvert extends Advert{
     }
 }
 
-class houseAdvert extends Advert{
-    #Класс объявлений для дома, включает в себя и покупку, и аренду
-    function __construct(...$args) {
-        parent::__construct(...$args);   
-   }
-    
+class HouseAdvert extends Advert{
+    //Класс объявлений для дома, включает в себя и покупку, и аренду    
 
-    function getTitle(){
-        #Реализация вывода объявления, условие для полной покупки, или аренды
+    function getTitle()
+    {
+        //Реализация вывода объявления, условие для полной покупки, или аренды
         if ($this->category == 'sale') {
             echo "Продам $this->rooms-комный дом за $this->price тенге" . PHP_EOL;
         } elseif ($this->category == "rent"){
             echo "Сдам $this->rooms-комнатный дом за $this->price тенге в " .countPeriodWord() . PHP_EOL;
         }
     }}
-    function number_format_short( $num, $okr = 1 ) {
-        #Округление чисел (регулярные выражения? не, не слышал)
+    function number_format_short( $num, $okr = 1 ): string
+    {
+        //Округление чисел (регулярные выражения? не, не слышал)
         if ($num < 900) {
             $n_format = number_format($num, $okr);
             $suffix = '';
@@ -143,36 +158,37 @@ class houseAdvert extends Advert{
         return $n_format . $suffix;
     }
 
-    #Стартовый словарь/ассоциативный массив
+    //Стартовый словарь/ассоциативный массив
     $adverts = [
         ['rooms' => 5, 'category' => 'sale', 'price' => 55000000, 'type' => 'dom'],
         ['rooms' => 2, 'category' => 'sale', 'price' => 21500000, 'type' => 'kvartira'],
         ['rooms' => 2, 'category' => 'rent', 'price' => 200000, 'type' => 'kvartira', 'period' => 'month'],
-        ['rooms' => 1, 'category' => 'rent', 'price' => 15000, 'type' => 'kvartira', 'period' => 'week'], #Поменял day на week чтобы протестить новый функционал
+        ['rooms' => 1, 'category' => 'rent', 'price' => 15000, 'type' => 'kvartira', 'period' => 'week'], //Поменял day на week чтобы протестить новый функционал
     ];
 
-#Проходимся циклом
+//Проходимся циклом
 foreach ($adverts as $innerarr) {
-    #В зависимости от значения ключа, вызываем инициализацию соответствующего класса и присвоением полей
+    //В зависимости от значения ключа, вызываем инициализацию соответствующего класса и присвоением полей
     if ($innerarr['type'] == "kvartira"){
         if(isset($innerarr['period'])){
-            $FlatOrHouseIDK = new flatAdvert($innerarr['rooms'],$innerarr['category'],number_format_short($innerarr['price']),$innerarr['type'],$innerarr['period']);
+            $flatOrHouseIDK = new FlatAdvert($innerarr);
       
         }
         else{
-            $FlatOrHouseIDK = new flatAdvert($innerarr['rooms'],$innerarr['category'],number_format_short($innerarr['price']),$innerarr['type']);
+            $flatOrHouseIDK = new FlatAdvert($innerarr);
         }
         }
     elseif ($innerarr['type'] == "dom"){
         if(isset($innerarr['period'])){
-            $FlatOrHouseIDK = new houseAdvert($innerarr['rooms'],$innerarr['category'],number_format_short($innerarr['price']),$innerarr['type'],$innerarr['period']);
+            $flatOrHouseIDK = new HouseAdvert($innerarr);
       
         }
         else{
-            $FlatOrHouseIDK = new houseAdvert($innerarr['rooms'],$innerarr['category'],number_format_short($innerarr['price']),$innerarr['type']);
+            $flatOrHouseIDK = new HouseAdvert($innerarr);
         }};
 
-#Вызываем наш долгожданный метод и радуемся жизни
-$FlatOrHouseIDK->getTitle();}
+//Вызываем наш долгожданный метод и радуемся жизни
+    $flatOrHouseIDK->getTitle();
+}
 
 ?>

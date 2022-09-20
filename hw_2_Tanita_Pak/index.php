d70f9ddf41b802efb740f1461a20371334751442
Tanita <tanita.pak@gmail.com>
2022-09-21 02:27:58 +0600

    выполнила задание №2

---
+++ hw_2_Tanita_Pak/index.php
@@ -0,0 +1,79 @@
+<?php
+
+$adverts = [
+    ['rooms' => 5, 'category' => 'sale', 'price' => 55000000, 'type' => 'dom'],
+    ['rooms' => 2, 'category' => 'sale', 'price' => 21500000, 'type' => 'kvartira'],
+    ['rooms' => 2, 'category' => 'rent', 'price' => 200000, 'type' => 'kvartira', 'period' => 'month'],
+    ['rooms' => 1, 'category' => 'rent', 'price' => 15000, 'type' => 'kvartira', 'period' => 'day'],
+];
+
+class Advert
+{
+    private $rooms;
+    public $category;
+    public $price;
+    public $type;
+
+    public function __construct($rooms, $category, $price, $type)
+    {
+        $this->rooms = $rooms;
+        $this->category = $category;
+        $this->price = $price;
+        $this->type = $type;
+    }
+
+    private function get_category()
+    {
+        if ($this->category == "sale") {
+            return "Продам";
+        } elseif ($this->category == "rent") {
+            return "Сдам";
+        }
+    }
+
+    private function get_type()
+    {
+        if ($this->type == "dom") {
+            return "дом";
+        } elseif ($this->type == "kvartira") {
+            return "квартиру";
+        }
+    }
+
+    private function get_ends()
+    {
+        if ($this->type == "dom") {
+            return "ый";
+        } elseif ($this->type == "kvartira") {
+            return "ую";
+        }
+    }
+
+    private function parse_price($size)
+    {
+        if ((strlen($this->price) > 6)) {
+            $postfix = "млн";
+            $i = 0;
+            while ($size >= 1000) {
+                $i++;
+                $size = $size / 1000;
+            }
+            $size = round($size * 100) / 100;
+            return $size . ' ' . $postfix;
+        } else {
+            return $this->price;
+        }
+    }
+
+    public function getTitle()
+    {
+        return $this->get_category() . " " . $this->rooms . "-комнатн" . $this->get_ends() . " " . $this->get_type()  . " " . "за"  . " " . $this->parse_price($this->price) . " " . "тг" . "\n";
+    }
+}
+
+for ($i = 0; $i < count($adverts); $i++) {
+    $new_arr = $adverts[$i];
+    $values = array_values($new_arr);
+    $a = new Advert($values[0], $values[1], $values[2], $values[3]);
+    echo $a->getTitle();
+}

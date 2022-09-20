d97ea0c4c4b2969c7658e2d4d67c1cb833a88527
Temirlan Shalqarov <Temirlan200312@gmail.com>
2022-09-20 21:15:48 +0600

    Добавил функцию readiblePrice в методы основного класса

--- advert.php
+++ advert.php
@@ -11,23 +11,39 @@ abstract class Advert
     protected float $price;
     protected string $type;
 
-    public function setRooms(int $rooms)
+    public function setRooms(int $rooms): void
     {
         $this->rooms = $rooms;
     }
-    public function setPrice(float $price)
+    public function setPrice(float $price): void
     {
         $this->price = $price;
     }
-    public function setType($type)
+    public function setType($type): void
     {
         $this->type = $type;
+    }
+    protected function readiblePrice(): string
+    {
+        if ($this->price < 1000000) {
+            return (string)$this->price;
+        }
+        if ($this->price >= 1000000 && $this->price < 1000000000) {
+            $this->price = round($this->price / 1000000, 1);
+
+            return "{$this->price} млн";
+        }
+        if ($this->price >= 1000000000 && $this->price < 1000000000000) {
+            $this->price = round($this->price / 1000000000);
+            return "{$this->price} млрд";
+        }
+        return sprintf('%d%s', floor($this->price / 1000000000000), ' трлн+');
     }
 }
 
 class Sale extends Advert implements Title
 {
-    public function getTitle()
+    public function getTitle(): void
     {
         if ($this->type === "kvartira") {
             print_r("Продам квартиру" . PHP_EOL);
@@ -35,8 +51,7 @@ class Sale extends Advert implements Title
             print_r("Продам дом" . PHP_EOL);
         }
         print_r("Количество комнат: {$this->rooms}" . PHP_EOL);
-        $newPrice = readiblePrice($this->price);
-        print_r("Цена: {$newPrice} тг." . PHP_EOL);
+        print_r("Цена: {$this->readiblePrice()} тг." . PHP_EOL);
         print_r("============" . PHP_EOL);
     }
 }
@@ -45,11 +60,20 @@ class Rent extends Advert implements Title
 {
     protected string $period;
 
-    public function setPeriod($period)
+    public function setPeriod($period): void
     {
         $this->period = $period;
     }
-    public function getTitle()
+
+    private function readiblePeriod(): string
+    {
+        if ($this->period === "посуточно") {
+            return "в сутки";
+        }
+        return "в мес";
+    }
+
+    public function getTitle(): void
     {
         if ($this->type === "kvartira") {
             print_r("Сдам квартиру " . $this->period . PHP_EOL);
@@ -57,37 +81,7 @@ class Rent extends Advert implements Title
             print_r("Сдам дом " . $this->period . PHP_EOL);
         }
         print_r("Количество комнат: {$this->rooms}" . PHP_EOL);
-        $newPrice = readiblePrice($this->price);
-        $newPeriod = $this->readiblePeriod($this->period);
-        print_r("Цена: {$newPrice} тг. {$newPeriod}" . PHP_EOL);
+        print_r("Цена: {$this->readiblePrice()} тг. {$this->readiblePeriod()}" . PHP_EOL);
         print_r("============" . PHP_EOL);
     }
-
-    private function readiblePeriod(string $period): string
-    {
-        if ($period === "посуточно") {
-            return "в сутки";
-        }
-        return "в мес";
-    }
-}
-
-function readiblePrice(float $price): string
-{
-    if ($price < 1000000) {
-        return (string)$price;
-    }
-
-    if ($price >= 1000000 && $price < 1000000000) {
-        $price = round($price / 1000000, 1);
-
-        return "{$price} млн";
-    }
-
-    if ($price >= 1000000000 && $price < 1000000000000) {
-        $price = round($price / 1000000000);
-        return "{$price} млрд";
-    }
-
-    return sprintf('%d%s', floor($price / 1000000000000), ' трлн+');
 }

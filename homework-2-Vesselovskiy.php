27b3e8da61971ae5ea39914c7860b58881ff15f3
Yevgeniy <vgenkon@gmail.com>
2022-09-21 17:30:52 +0600

    выполняю задание 2

--- homework-2-Vesselovskiy.php
+++ homework-2-Vesselovskiy.php
@@ -1,60 +1,47 @@
 <?php
 
-$adverts = [
-    ['rooms' => 5, 'category' => 'sale', 'price' => 55000000, 'type' => 'dom'],
-    ['rooms' => 2, 'category' => 'sale', 'price' => 21500000, 'type' => 'kvartira'],
-    ['rooms' => 2, 'category' => 'rent', 'price' => 200000, 'type' => 'kvartira', 'period' => 'month'],
-    ['rooms' => 1, 'category' => 'rent', 'price' => 15000, 'type' => 'kvartira', 'period' => 'day'],
-];
-class Adverts
+class Advert
 {
     private $rooms;
     private $category;
     private $price;
     private $type;
     private $period;
-    public function __construct()
+    public function __construct($rooms,  $category,  $price,  $type, $period)
     {
-        $this->rooms = 'rooms';
-        $this->category = 'category';
-        $this->price = 'price';
-        $this->type = 'type';
-        $this->period = 'period';
+        $this->rooms = $rooms;
+        $this->category = $category;
+        $this->price = $price;
+        $this->type = $type;
+        $this->period = $period;
     }
-    public function showPrivate()
+    public function getTitle()
     {
-        echo "$this->rooms </br> $this->category </br> $this->price </br> $this->type </br> $this->period";
-    }
-    public function getTitle($input)
-    {
-        foreach ($input as $advert) {
-            if ($advert['type'] == 'dom') {
-                $this->rooms = $advert['rooms'] . '-комнатный ';
-                $this->type = 'дом';
-            } else {
-                $this->rooms = $advert['rooms'] . '-комнатную ';
-                $this->type = 'квартиру';
-            }
-            if ($advert['category'] == 'rent') {
-                $this->category = 'Сдам ';
-                $this->price = number_format($advert['price'], 0, '', ' ');
-            } else {
-                $this->category = 'Продам ';
-                $this->price = number_format($advert['price'] / 1000000, 1);
-            }
-            if (isset($advert['period'])  && ($advert['period'] == 'day')) {
-                $this->period = 'в сутки';
-            } else {
-                $this->period = 'в месяц';
-            }
-            if ($advert['category'] == 'sale') {
-                echo $this->category, $this->rooms, $this->type, ' за ', $this->price, ' млн. тг </br>';
-            } else {
-                echo $this->category, $this->rooms, $this->type, ' за ', $this->price, ' тг в ', $this->period, '</br>';
-            }
+        if ($this->type == 'dom') {
+            return "Продам " . $this->rooms . "-комнатный дом за " . number_format($this->price / 1000000, 1) . ' млн. тг' . "<br>";
+        }
+        if ($this->type == 'kvartira' && $this->period == NULL) {
+            return "Продам " . $this->rooms . "-комнатную квартиру за " . number_format($this->price / 1000000, 1) . ' млн. тг' . "<br>";
+        }
+        if ($this->period == 'month') {
+            return "Сдам " . $this->rooms . "-комнатную квартиру за " . number_format($this->price, 0, '', ' ') . " тг в месяц" . "<br>";
+        } else {
+            return "Сдам " . $this->rooms . "-комнатную квартиру за " . number_format($this->price, 0, '', ' ') . " тг в сутки" . "<br>";
         }
     }
 }
-$titles = new Adverts;
-$titles->getTitle($adverts);
-// $titles->showPrivate();
+$adverts = [
+    ['rooms' => 5, 'category' => 'sale', 'price' => 55000000, 'type' => 'dom'],
+    ['rooms' => 2, 'category' => 'sale', 'price' => 21500000, 'type' => 'kvartira'],
+    ['rooms' => 2, 'category' => 'rent', 'price' => 200000, 'type' => 'kvartira', 'period' => 'month'],
+    ['rooms' => 1, 'category' => 'rent', 'price' => 15000, 'type' => 'kvartira', 'period' => 'day'],
+];
+foreach ($adverts as $advert) {
+    if ($advert['category'] == 'sale') {
+        $advert = new Advert($advert['rooms'], $advert['category'], $advert['price'], $advert['type'], NULL);
+        echo $advert->getTitle();
+    } else {
+        $advert = new Advert($advert['rooms'], $advert['category'], $advert['price'], $advert['type'], $advert['period']);
+        echo $advert->getTitle();
+    }
+}

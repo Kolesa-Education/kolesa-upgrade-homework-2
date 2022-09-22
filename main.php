d605402332ea2d4b438471a8fde59b3579af3239
Aishuak <aishuak99@mail.ru>
2022-09-22 11:26:41 +0600

    little bit of corrections

--- main.php
+++ main.php
@@ -8,10 +8,6 @@ $adverts = [
     ['rooms' => 1, 'category' => 'rent', 'price' => 150000, 'type' => 'kvartira', 'period' => 'day'],
 ];
 
-// you can create  object by passing arguments directly like below:
-// $myobj = new Advert(5, 'sale', 55000000, 'dom', 'month');
-
-// or you can pass dictionary
 foreach ($adverts as $ad) {
     if (isset($ad['period'])) {
         $ad_obj = new Advert($ad['rooms'], $ad['category'], $ad['price'], $ad['type'], $ad['period'],);
